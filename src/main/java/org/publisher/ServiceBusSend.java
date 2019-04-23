638e47a067212fa090b2ccad9f750db7f9d9c1bb
Kosala <kosala.amarasinghe@skipthedishes.ca>
2019-04-23 04:48:33 -0600

    master- renaming to CloudUtils

--- src/main/java/org/publisher/ServiceBusSend.java
+++ src/main/java/org/publisher/ServiceBusSend.java
@@ -15,14 +15,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
-public class ServiceBusSend implements Publisher {
+public final class ServiceBusSend implements Publisher {
 
-    private static final Gson gson = new Gson();
+    private static final Gson GSON = new Gson();
 
-    public void publish(String queueName, String json) throws ServiceBusException, InterruptedException {
-        TopicClient sendClient;
+    public void publish(final String queueName, final String json) throws ServiceBusException, InterruptedException {
         String connectionString = "Endpoint=sb://<NameOfServiceBusNamespace>.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=<AccessKey>";
-        sendClient = new TopicClient(new ConnectionStringBuilder(connectionString, queueName));
+        TopicClient sendClient = new TopicClient(new ConnectionStringBuilder(connectionString, queueName));
         sendMessagesAsync(sendClient, json).thenRunAsync(() -> sendClient.closeAsync());
     }
 
@@ -30,16 +29,16 @@ public class ServiceBusSend implements Publisher {
 
     }
 
-    static CompletableFuture<Void> sendMessagesAsync(TopicClient sendClient, String json) {
+    private CompletableFuture<Void> sendMessagesAsync(TopicClient sendClient, String json) {
         List<HashMap<String, String>> data =
-                gson.fromJson(json,
-                        new TypeToken<List<HashMap<String, String>>>() {
+                GSON.fromJson(json,
+                              new TypeToken<List<HashMap<String, String>>>() {
                         }.getType());
 
         List<CompletableFuture> tasks = new ArrayList<>();
         for (int i = 0; i < data.size(); i++) {
             final String messageId = Integer.toString(i);
-            Message message = new Message(gson.toJson(data.get(i), Map.class).getBytes(StandardCharsets.UTF_8));
+            Message message = new Message(GSON.toJson(data.get(i), Map.class).getBytes(StandardCharsets.UTF_8));
             message.setContentType("application/json");
             message.setLabel("Scientist");
             message.setMessageId(messageId);
